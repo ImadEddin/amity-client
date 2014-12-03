892741918fb96897ee3c9fbafe23a305ee5f8018
Christoph <christoph@webvariants.de>
2014-12-03 22:16:42 +0100

    reworked the client to rely on the plugin to be present

--- src/Client.php
+++ src/Client.php
@@ -5,31 +5,44 @@
 
 namespace dialogue1\amity\API;
 
+use dialogue1\amity\API\Guzzle\RequestSigner;
+use Guzzle\Http\Client as GuzzleHttpClient;
 use Guzzle\Http\ClientInterface;
-use Guzzle\Http\Message\RequestInterface;
 use Guzzle\Http\Message\EntityEnclosingRequestInterface;
+use Guzzle\Http\Message\RequestInterface;
 
 class Client {
 	const API_VERSION = 'v2';
 
 	protected $http;
-	protected $clientID;
-	protected $apiKey;
 
-	public function __construct(ClientInterface $httpClient, $amityClientID, $amityApiKey) {
-		$this->http     = $httpClient;
-		$this->clientID = $amityClientID;
-		$this->apiKey   = $amityApiKey;
+	public function __construct(ClientInterface $httpClient) {
+		$this->http = $httpClient;
+	}
+
+	public static function create($host, $clientID, $apiKey) {
+		$guzzle = new GuzzleHttpClient('http://'.$host.'/');
+		$signer = new RequestSigner($clientID, $apiKey);
+
+		$guzzle->addSubscriber($signer);
+
+		return new static($guzzle);
 	}
 
 	public function checkConnection() {
 		$response = $this->request('GET', '/');
 	}
 
-	protected function request($method, $uri, array $query = array(), array $data = array()) {
+	protected function request($method, $uri, array $query = array(), array $body = array()) {
 		$fullUri = '/api/'.self::API_VERSION.$uri;
-		$request = $this->http->createRequest($method, $fullUri);
+		$body    = json_encode($body);
+		$headers = array(
+			'Content-Type' => 'application/json; charset=UTF-8',
+			'Accept'       => 'application/json',
+			'User-Agent'   => 'dialogue1/amity-client 1.0'
+		);
 
+		$request = $this->http->createRequest($method, $fullUri, $headers, $body);
 		$request->getQuery()->replace($query);
 
 		return $request->send();
