f8095ba9fe64890e8b9a04245c3dfa9e6e61d89c
Christoph <christoph@webvariants.de>
2014-12-04 02:43:30 +0100

    implemented exception handling and first contact-related wrappers

--- src/Client.php
+++ src/Client.php
@@ -7,8 +7,10 @@ namespace dialogue1\amity\API;
 
 use Guzzle\Http\Client as GuzzleHttpClient;
 use Guzzle\Http\ClientInterface;
+use Guzzle\Http\Exception as GuzzleEx;
 use Guzzle\Http\Message\EntityEnclosingRequestInterface;
 use Guzzle\Http\Message\RequestInterface;
+use Symfony\Component\HttpKernel\Exception as SymfonyEx;
 
 class Client {
 	const API_VERSION = 'v2';
@@ -29,7 +31,53 @@ class Client {
 	}
 
 	public function checkConnection() {
-		$response = $this->request('GET', '/');
+		$this->request('GET', '/');
+
+		return true;
+	}
+
+	public function getContacts($page = 5000, $size = null, $email = null, $substring = null, $gender = null, $active = null) {
+		$query = array();
+
+		if ($page   !== null) $query['page']   = (int) $page;
+		if ($size   !== null) $query['size']   = (int) $size;
+		if ($active !== null) $query['active'] = $active ? 1 : 0;
+
+		if ($gender !== null) {
+			if (!in_array($gender, array('f', 'm', 'x'), true)) {
+				throw new \InvalidArgumentException('$gender must be one of [f, m, x].');
+			}
+
+			$query['gender'] = $gender;
+		}
+
+		if ($email !== null) {
+			$query['email'] = $email;
+
+			if ($substring) {
+				$query['substring'] = 1;
+			}
+		}
+
+		$data = $this->request('GET', '/contacts', $query);
+
+		return $data['data'];
+	}
+
+	public function getContact($id) {
+		$data = $this->request('GET', '/contacts/'.$id);
+
+		return $data['data'];
+	}
+
+	public function createContact(array $contactData, array $lists = array(), array $events = array()) {
+		$data = $this->request('POST', '/contacts', array(), array(
+			'contact' => $contactData,
+			'lists'   => $lists,
+			'events'  => $events
+		));
+
+		return $data['data'];
 	}
 
 	protected function request($method, $uri, array $query = array(), array $body = array()) {
@@ -44,6 +92,49 @@ class Client {
 		$request = $this->http->createRequest($method, $fullUri, $headers, $body);
 		$request->getQuery()->replace($query);
 
-		return $request->send();
+		try {
+			$response = $request->send();
+			$data     = @json_decode($response->getBody(true), true);
+
+			if (!is_array($data)) {
+				throw new SymfonyEx\ServiceUnavailableHttpException(null, 'API did not return JSON.');
+			}
+
+			return $data;
+		}
+		catch (GuzzleEx\BadResponseException $e) {
+			throw $this->buildException($e);
+		}
+	}
+
+	protected function buildException(GuzzleEx\BadResponseException $exception) {
+		$response = $exception->getResponse();
+		$body     = $response->getBody(true);
+		$message  = $body;
+		$code     = null;
+
+		if ($response->isContentType('application/json')) {
+			$data    = @json_decode($body, true);
+			$message = isset($data['message']) ? $data['message'] : '(no error message given)';
+
+			if (isset($data['status'])) {
+				$status = (int) $data['status'];
+				$code   = $status - ((int) ($status / 1000)) * 1000;
+			}
+		}
+
+		switch ($response->getStatusCode()) {
+			case 400: return new SymfonyEx\BadRequestHttpException($message, null, $code);
+			case 401:
+			case 403: return new SymfonyEx\AccessDeniedHttpException($message, null, $code);
+			case 404: return new SymfonyEx\NotFoundHttpException($message, null, $code);
+			case 405: return new SymfonyEx\MethodNotAllowedHttpException(array(), $message, null, $code);
+			case 406: return new SymfonyEx\NotAcceptableHttpException($message, null, $code);
+			case 409: return new SymfonyEx\ConflictHttpException($message, null, $code);
+			case 415: return new SymfonyEx\UnsupportedMediaTypeHttpException($message, null, $code);
+			case 500:
+			case 503: return new SymfonyEx\ServiceUnavailableHttpException(null, $message, null, $code);
+			default:  return new SymfonyEx\HttpException(500, $message, null, array(), $code);
+		}
 	}
 }
